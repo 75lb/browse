f964e06e7563f0ba81a5c6a3c0f369790c78af91
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-27 10:26:49 +0100

    ca now detaches correctly

--- script/ca.js
+++ script/ca.js
@@ -2,7 +2,7 @@
 var cp = require("child_process"),
     caPath = "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary";
 
-cp.spawn(caPath, process.argv.slice(2), { detached: true }).unref();
+cp.spawn(caPath, process.argv.slice(2), { detached: true, stdio: "ignore" }).unref();
 setTimeout(function(){
-    cp.spawn(__dirname + "/fullscreen.applescript", [ "Google Chrome Canary" ]);
+    cp.spawn(__dirname + "/fullscreen.applescript", [ "Google Chrome Canary" ], { stdio: "inherit" });
 }, 1000);
