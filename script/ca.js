58b8d5825ac4646a7aa748c0cef9081d4303a0ae
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-01 10:17:24 +0100

    wait longer

--- script/ca.js
+++ script/ca.js
@@ -5,4 +5,4 @@ var cp = require("child_process"),
 cp.spawn(caPath, process.argv.slice(2), { detached: true, stdio: "ignore" }).unref();
 setTimeout(function(){
     cp.spawn(__dirname + "/fullscreen.applescript", [ "Google Chrome Canary" ], { stdio: "inherit" });
-}, 1000);
+}, 2000);
